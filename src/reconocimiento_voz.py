b7e0b41985454f7297f4ddd93964509ef9d062ea
danieeeld2 <danieeeld2@gmail.com>
2024-05-04 11:53:02 +0200

    Reconocer voz del usuario closes #4

--- src/reconocimiento_voz.py
+++ src/reconocimiento_voz.py
@@ -1,15 +1,46 @@
 import speech_recognition as sr
 import sounddevice
 import pyaudio
+import time
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source, phrase_time_limit=5)
+def comporbar_transcripcion(transcripcion):
+    clave = "modo"
+    if transcripcion is not None:
+        if clave in transcripcion:
+            return True
+    return False
 
-try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language='es-ES'))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+def escuchar(cola1, cola2):
+    r = sr.Recognizer()
+    while True:
+        if not cola2.empty():
+            with sr.Microphone() as source:
+                audio = r.listen(source, phrase_time_limit=4)
+            try:
+                transcripcion = r.recognize_google(audio, language='es-ES')
+            except sr.UnknownValueError:
+                transcripcion = None
+            except sr.RequestError:
+                transcripcion = None
+            if transcripcion is not None:
+                print("Google Speech Recognition thinks you said " + transcripcion)
+            if comporbar_transcripcion(transcripcion):
+                cola1.put(transcripcion)
+                time.sleep(2)
+
+
+def test():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source, phrase_time_limit=2)
+
+    try:
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language='es-ES'))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+if __name__ == "__main__":
+    test()
